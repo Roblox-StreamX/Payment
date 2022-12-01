c7ee5e3b97170f7e18247639681bc2b091c178b5
iiPython <ben@iipython.cf>
2022-11-30 22:04:14 -0600

    Update CLI to work with MongoDB

--- streamx-cli.py
+++ streamx-cli.py
@@ -3,9 +3,9 @@
 
 # Modules
 import os
+import json
 import shlex
 import requests
-from datetime import timedelta, date
 try:
     from rich import print
 
@@ -13,25 +13,30 @@ except ImportError:
     pass  # Rich is nice, but not required
 
 # Initialization
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 print(f"StreamX Payment CLI v{__version__}\nCopyright (c) 2022 StreamX Developers\n")
 
 # Fetch URL + API key
 base_url = input("StreamX Purchase Server URI (quantumpython.xyz): ") or "https://streamxpayment.quantumpython.xyz"
-apikeys = [f for f in os.listdir() if ".pem" in f] + (["db/server.pem"] if os.path.isfile("db/server.pem") else [])
-if not apikeys:
-    api_key = input("StreamX API Key: ")
+streamx_jsonfile = os.path.abspath(os.path.join(os.path.dirname(__file__), ".streamx.json"))
+if os.path.isfile(streamx_jsonfile):
+    with open(streamx_jsonfile, "r") as fh:
+        data = json.loads(fh.read())
+
+    authkey = data["authkey"]
 
 else:
-    with open(apikeys[0], "r") as fh:
-        api_key = fh.read().strip("\n")
+    authkey = input("StreamX Authentication Key: ")
+    if (input("Save these credentials (y/N)? ") or "n").lower() == "y":
+        with open(streamx_jsonfile, "w+") as fh:
+            fh.write(json.dumps({"authkey": authkey}))
 
 def make_request(endpoint: str, method: str = "get", js: bool = True, **kwargs) -> dict:
     d = {
         "get": requests.get,
         "post": requests.post
     }[method](f"{base_url.rstrip('/')}{endpoint}", headers = {
-        "X-StreamX-Token": api_key
+        "X-StreamX-Token": authkey
     }, **kwargs)
     return d.json() if js else d
 
@@ -52,20 +57,17 @@ def function_help(a: list) -> None:
     msg = "\n".join(x.split("~ ")[1] for x in """
     ~ Commands
     ~     help                    -- Shows this message
-    ~     get-active              -- Lists all active API keys
-    ~     is-active <key>         -- Checks if an API key is valid
-    ~     delete <userid>         -- Removes a user and their API key
+    ~     active <key>            -- Checks if an API key is valid
+    ~     delete <userid>         -- Removes all stored data abouta  user
     ~     activate <userid>       -- Activate/reactivate a user's API status
     ~     info <userid>           -- Shows information on a user
+    ~     apikeys <userid>        -- Fetch all API keys of a user
     ~     clear                   -- Clears the screen
     ~     exit                    -- Exits the CLI
     """.split("\n") if x.strip())
     return print(msg)
 
 def function_active(a: list) -> None:
-    print(make_request("/active"))
-
-def function_isactive(a: list) -> None:
     if not a:
         return print("[red]\\[streamx]: missing argument <key>[/]")
 
@@ -75,7 +77,7 @@ def function_delete(a: list) -> None:
     if not a:
         return print("[red]\\[streamx]: missing argument <userid>[/]")
 
-    print(make_request("/delete", method = "post", data = {"userid": a[0]}))
+    print(make_request("/delete", method = "post", json = {"userid": a[0]}))
 
 def function_info(a: list) -> None:
     if not a:
@@ -83,20 +85,26 @@ def function_info(a: list) -> None:
 
     print(make_request(f"/info/{a[0]}"))
 
+def function_apikeys(a: list) -> None:
+    if not a:
+        return print("[red]\\[streamx]: missing argument <userid>[/]")
+
+    print(make_request(f"/info/{a[0]}")["apikeys"])
+
 def function_activate(a: list) -> None:
     try:
         if not a:
             return print("[red]\\[streamx]: missing argument <userid>[/]")
 
-        opt, username = int(input("Activate (1) or reactivate (2)? ")), ""
+        opt, username = int(input("Activate (1) or extend quota (2)? ")), ""
         if opt not in [1, 2]:
             return print("[red]\\[streamx]: invalid option[/]")
 
         elif opt != 2:
             username = input("Roblox Username: ")
 
-        expires = int((date.today() + timedelta(days = int(input("Expires in (days from now): ")))).strftime("%s"))
-        print(make_request("/activate", method = "post", data = {
+        expires = int(input("Quota length (days): "))
+        print(make_request("/activate", method = "post", json = {
             "userid": a[0], "username": username, "expires": expires
         }))
 
@@ -104,18 +112,15 @@ def function_activate(a: list) -> None:
         return print("[red]\\[streamx]: invalid option[/]")
 
 def function_clear(a: list) -> None:
-    if os.name != "nt":
-        os.system("clear")
-    else:
-        os.system("cls")
+    os.system("clear" if os.name != "nt" else "cls")
 
 commands = {
     "help": function_help,
-    "get-active": function_active,
-    "is-active": function_isactive,
+    "active": function_active,
     "delete": function_delete,
     "activate": function_activate,
     "info": function_info,
+    "apikeys": function_apikeys,
     "clear": function_clear,
     "exit": lambda *a: exit()
 }
